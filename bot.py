04a3a4ef93ccb3b340def9e87ec21efe995a9d42
adipingo94-create <adipingo94@gmail.com>
2026-09-23 06:30:02 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ ARQUIVO = "resultados.json"
 if not TOKEN:
     raise RuntimeError("BOT_TOKEN não foi configurado.")
 
-app_web = Flask(**name**)
+app_web = Flask(__name__)
 
 telegram_app = Application.builder().token(TOKEN).build()
 
